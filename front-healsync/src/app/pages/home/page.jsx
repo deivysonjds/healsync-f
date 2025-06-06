5d5080402c45e05724375e805b98f5634c9f87ac
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-06-06 12:24:58 -0300

    added: unidades verification

--- front-healsync/src/app/pages/home/page.jsx
+++ front-healsync/src/app/pages/home/page.jsx
@@ -1,16 +1,47 @@
+"use client";
+import { useEffect, useState } from "react";
 import Fluxos from "../../components/FluxoTabs";
 import Header from "../../components/Header";
 import UnidadeTabs from "../../components/UnidadeTabs";
 import Wellcome from "../../components/Wellcome";
+import SemUnidade from "@/app/components/NoUnidade";
 
 export default function Home() {
+  const [unidades, setUnidades] = useState([]);
+
+  useEffect(() => {
+    async function fetchUnidades() {
+      try {
+        const res = await fetch("/api/unidades", {
+          //substituir pela URL do backend
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+        const data = await res.json();
+        setUnidades(data);
+      } catch (err) {
+        console.error("Erro ao buscar unidades:", err);
+      }
+    }
+
+    fetchUnidades();
+  }, []);
+
   return (
     <>
       <Header />
       <main>
         <Wellcome />
-        <UnidadeTabs />
-        <Fluxos />
+        {unidades.length === 0 ? (
+          <SemUnidade />
+        ) : (
+          <>
+            <UnidadeTabs unidades={unidades} />
+            <Fluxos />
+          </>
+        )}
       </main>
     </>
   );
